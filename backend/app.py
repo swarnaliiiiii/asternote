798a6b3bcd2d228857f812ae0c28b05fe2353657
Swarnali Bhattacharyya <oli2k3@gmail.com>
2025-03-01 19:59:36 +0530

    Add image upload functionality and integrate with Google Books API

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from blueprints.upload import upload_bp
 
 app = Flask(__name__)
+
+app.register_blueprint(upload_bp, url_prefix="/api")
 
 if(__name__ == '__main__'):
     app.run(debug=True)
--- backend/blueprints/upload.py
+++ backend/blueprints/upload.py
@@ -10,6 +10,13 @@ google_base_endpoint = "https://www.googleapis.com/books/v1/volumes"
 def upload():
     if "image" not in request.files:
         return jsonify({"error": "No image provided"})
+    image = request.files["image"]
+    image.save("image.jpg")
+    text = pytesseract.image_to_string(Image.open("image.jpg"))
+    response = requests.get(google_base_endpoint, params={"q": text})
+    book_data = response.json()
+
+    return jsonify({"query": text, "data": book_data})
 
     
     
